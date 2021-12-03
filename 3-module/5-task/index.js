c1cf4383cb5bd32ca8039dd4effc8020320fbea9
erastian <erastian@gmail.com>
2021-12-03 03:17:22 +0300

    Занятие 3: Функции и массивы => 5 tasks completed

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str
+    .split(' ')
+    .map(item => ((item.includes('.')) ? parseFloat(item) : parseInt(item)))
+    .filter(item => !isNaN(item));
+
+  return {min: Math.min(...arr), max: Math.max(...arr)};
 }
