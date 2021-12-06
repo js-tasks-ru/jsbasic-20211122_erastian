7c252316976f538564eca41308297b49310f7237
erastian <erastian@gmail.com>
2021-12-07 00:59:44 +0300

    Занятие 4: Знакомство с DOM => 3 tasks completed

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+
+  // Есть ли решение изящнее чем пучок if`ов?
+
+  for (const row of table.rows) {
+    if (row.parentNode.nodeName !== 'TBODY') continue;
+
+    let state = row.lastElementChild.dataset.available;
+
+    if (state === undefined) {
+      row.setAttribute('hidden', true);
+    }
+
+    if (state === 'false') {
+      row.classList.add('unavailable');
+    } else {
+      row.classList.add('available');
+    }
+
+    if (row.cells[2].textContent === 'm') {
+      row.classList.add('male');
+    } else if (row.cells[2].textContent === 'f') {
+      row.classList.add('female');
+    }
+
+    if (row.cells[1].textContent < 18) { row.style.textDecoration = 'line-through'; }
+  }
 }
